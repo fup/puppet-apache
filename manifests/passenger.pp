86c84384a1727685b287846fe710fa42c850378d
James Fryman <james@frymanet.com>
2011-09-21 14:03:52 -0500

    adding phusion passenger template and package install

---
+++ manifests/passenger.pp
@@ -0,0 +1,26 @@
+# Class: apache::passenger
+#
+# This class installs Phusion Passenger for Apache
+#
+# Parameters:
+# - $passenger_package
+#
+# Actions:
+#   - Install Apache PHP package
+#
+# Requires:
+#   Phusion Passenger Packages for your OS
+#   - RPM: http://passenger.stealthymonkeys.com/
+#
+# Sample Usage:
+#
+class apache::passenger {
+
+  include apache::params
+
+  @package { $apache::params::passenger_package:
+    ensure => present,
+    tag    => 'passenger-package',
+  }
+  Package<| tag == 'passenger-package' |>
+}
